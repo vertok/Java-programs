36f75868941e33e6b29ee9796a5b1dcda04c5dc8
Alexey Obukhov <34322911+vertok@users.noreply.github.com>
2017-12-11 09:56:37 +0100

    Add files via upload
    print this kind of muster 
    XXXXX. . . . .XXXXX
    XXXXX. . . . .XXXXX
    XXXXX. . . . .XXXXX
     . . . . .XXXXX . . . . . 
     . . . . .XXXXX . . . . . 
     . . . . .XXXXX . . . . . 
    XXXXX. . . . .XXXXX
    XXXXX. . . . .XXXXX
    XXXXX. . . . .XXXXX
    
    or like this 
    XX . . XX . . XX
     . . XX . . XX . .

--- Muster.java
+++ Muster.java
@@ -1,51 +1,77 @@
 package mein.paket;
 
 public class Muster {
+	static String[] temp;
 	
-	static String[][] temp, temp2;
-	public static void druckeMuster(int a, int b, int c, int d) {
-
-		temp = new String[c][d];
-		temp2 = new String[c][d];
-		for(int i=0; i<a;i++) {
-			System.out.println("");	
-			for(int j=0;j<b;j++) {
-				if((i+j)%2 == 0)
-					DruckX(c,d);
+	public static void DruckLineA(int a, int b, int c, int d) { // print line which starts like this: XXXX....XXXX.........
+		while(a!=0) {
+				if(a%2 == 0)
+					DruckX(c);
 				else
-					DruckPoint(c,d);
+					DruckPoint(c);
 				
-				
-			}}
-		}
-	public static void DruckPoint(int c, int d) {
-					for(int j1=0;j1<d;j1++) {
-					
-						//System.out.println("");					
-						for(int i1=0; i1<c;i1++) {
-							temp2[i1][j1] = ".";
-							System.out.print(temp2[i1][j1]);
-				} 
-						System.out.println("");
-				}	
-				}
-	public static void DruckX(int c, int d) {
-				for(int j1=0;j1<d;j1++) {
-				
-					//System.out.println("");
-					for(int i1=0; i1<c;i1++) {
-						temp[i1][j1] = "X";	
-						System.out.print(temp[i1][j1]);
+				a--;
 			}
-			System.out.println("");
+		System.out.println();
+		}
+	
+	public static void DruckLineB(int a, int b, int c, int d) { // print line which starts like this: ....XXXX....XXXX.........
+		while(a!=0) {
+				if(a%2 != 0)
+					DruckX(c);
+				else
+					DruckPoint(c);
+				
+				a--;
+			}
+		System.out.println();
+		}
+	
+	public static void DruckPoint(int c) {	//print line with length c from "."			
+						while(c != 0) {
+							System.out.print(".");
+							c--;
+				} 
+				}	
+	
+	public static void DruckX(int c) { //print line with length c from "X"	
+					while(c != 0) {
+						System.out.print("X");
+						c--;
 			}
 			}
 
+	public static void DruckBlockLine(int a, int b, int c, int d,boolean beginneX ) {
+		while(d!=0) {
+			if(beginneX)
+				DruckLineA(a,b,c,d);
+			else
+				DruckLineB(a,b,c,d);
+			d--;
+		}
+	}
+	
+	public static void DruckMuster(int a, int b, int c, int d) {
+		boolean beginneX;  // this variable we need to clear whether we will start the Line/BlockLine with "X" or "."
+		if(b%2 == 0)  // our boolean variable works with b. but if b%2 will be 1( if b is odd), then we need to keep it in mind and do something like this 
+			beginneX = true;
+		else
+			beginneX = false;
+		while(b != 0) {
+			if(b%2 == 0)
+				DruckBlockLine(a, b, c, d, beginneX);
+			else
+				DruckBlockLine(a, b, c, d,!beginneX);
+			b--;
+		}
+		System.out.println();
+	}
+		
+	
 	public static void main(String[] args) {
 
-		druckeMuster(6,4,5,3);
-
-
+		DruckMuster(6,4,5,3);
+		DruckMuster(8,5,2,1);
 	}
 
 }
